31cf577a05b878d2b5dd7f54f2ce3ee89d10f336
Munkhtegsh <munkhtegsh.m@gmail.com>
2018-10-29 12:20:00 -0700

    Change flag images when user clicks

--- src/App.js
+++ src/App.js
@@ -5,24 +5,46 @@ import axios from 'axios';
 class App extends Component {
 
   state = {
-    players: []
+    flags: [],
+    flag: {},
+    idx: 1
   }
 
   componentDidMount = () => {
-    axios.get("https://swapi.co/api/starships")
-      .then(res => {
-        this.setState({players: res.data})
+    axios.get("https://restcountries.eu/rest/v2/all").then(res => {
+      this.setState({
+        flags: res.data.slice(0, 10),
+        flag: res.data[0]
+      });
+    });
+  }
+
+  handleClick = () => {
+    if (this.state.idx <= 9) {
+      this.setState((state) => {
+        return {
+          flag: state.flags[this.state.idx],
+          idx: state.idx + 1
+        }
       })
+      if (this.state.idx === 9) {
+        this.setState({ idx: 0 })
+      }
+    } 
   }
 
   render() {
-
-    console.log(this.state.players.results)
+    console.log(this.state.idx)
     return (
       <div className="App">
-        <p>
-          Create react app
-          </p>
+        <h1>  
+          Flag gallery
+        </h1>
+        
+        <div onClick={() => this.handleClick()}>
+          <img src={this.state.flag.flag} alt='flag' />
+          <p>{this.state.flag.name}</p>
+        </div>
       </div>
     );
   }
