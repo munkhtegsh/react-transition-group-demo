6b225bf551fce7d82834f000b0cd52ca693960e7
Munkhtegsh <munkhtegsh.m@gmail.com>
2018-10-29 09:20:53 -0700

    Fetch starships from star wars API and save it in the state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  state = {
+    players: []
+  }
+
+  componentDidMount = () => {
+    axios.get("https://swapi.co/api/starships")
+      .then(res => {
+        this.setState({players: res.data})
+      })
+  }
+
   render() {
+
+    console.log(this.state.players.results)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+        <p>
+          Create react app
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
     );
   }
